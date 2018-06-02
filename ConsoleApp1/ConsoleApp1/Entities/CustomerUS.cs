92568ddd081474c8c94ca4e86a6b7c2fe12613cd
Kumar <kumar.sendhi@gmail.com>
2018-06-02 09:09:24 +0000

    70-532 StorageTable - Insert Row

---
+++ ConsoleApp1/ConsoleApp1/Entities/CustomerUS.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    class CustomerUS : TableEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public CustomerUS(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.PartitionKey = "US";
+            this.RowKey = email;
+        }
+    }
+}
